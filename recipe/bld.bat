8b8a375d1c75ad3cbe491da2061e85fa7dd9ad11
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2023-04-28 16:05:54 +0300

    draco 1.5.6 (#1)
    * Add release 1.5.6
    
    * Add selectors
    
    * Use ninja on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,34 @@
+:: cmd
+echo "Building %PKG_NAME%."
+
 mkdir build
 cd build
+if errorlevel 1 exit /b 1
 
-cmake -G "NMake Makefiles" ^
+:: Generate the build files.
+echo "Generating the build files..."
+cmake .. %CMAKE_ARGS% ^
+      -G"Ninja" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
       -DCMAKE_INCLUDE_PATH="%INCLUDE_INC%" ^
       -DBUILD_SHARED_LIBS=OFF ^
       %SRC_DIR%
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
-nmake
-if errorlevel 1 exit 1
+:: Build.
+echo "Building..."
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit /b 1
 
-nmake install
-if errorlevel 1 exit 1
 
+:: Install.
+echo "Installing..."
+ninja install
+if errorlevel 1 exit /b 1
+
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
